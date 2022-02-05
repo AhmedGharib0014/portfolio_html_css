ba1fc0338776678cd2e2b641862cd2b548d948b5
Ahmed Gharib <ahmedgharib0014@gmail.com>
2022-02-05 22:38:55 +0200

    first responsive web page with only html and css

--- app.js
+++ app.js
@@ -4,11 +4,29 @@
 
 var hum = document.querySelector('#header .header .icon ');
 var hidderList = document.querySelector('#header .header .header-list ');
+var header = document.querySelector('#header');
+var linkes = document.querySelectorAll('#header .header .header-list ul li a');
 
 
+
+linkes.forEach((element) => {
+    element.addEventListener(('click'), () => {
+        hum.classList.toggle('active');
+        hidderList.classList.toggle('active');
+    })
+})
 
 
 hum.addEventListener('click', () => {
     hum.classList.toggle('active');
     hidderList.classList.toggle('active');
 });
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+})
